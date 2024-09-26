b8f54a70d5bab9294757502bc83497bf00f104a2
Anshcool5 <anshul5@ualberta.ca>
2024-09-26 03:02:00 -0600

    Created an abstract class with constructor

--- app/src/main/java/com/example/aurora/Shape.java
+++ app/src/main/java/com/example/aurora/Shape.java
@@ -1,2 +1,12 @@
-package com.example.aurora;public class Shape {
+package com.example.aurora;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    // Constructor to initialize x and y
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
