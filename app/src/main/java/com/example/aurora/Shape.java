b9930dbd55ccfacde975949b2f84adc791630149
Dhenamuthan Ragu <dhenamuthan@gmail.com>
2024-09-26 23:39:29 -0600

    changed color in shape.java

--- app/src/main/java/com/example/aurora/Shape.java
+++ app/src/main/java/com/example/aurora/Shape.java
@@ -3,7 +3,7 @@ package com.example.aurora;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "brown";
+    String color = "grey";
 
     public Shape(){
         
